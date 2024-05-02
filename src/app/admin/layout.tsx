f345b560805b40b3435f7ff9135afa9dabc563ae
calrosdsa <carlosduram71@gmail.com>
2024-05-02 18:06:30 -0400

    changes

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -17,7 +17,8 @@ import ButtonIcon from '@/components/util/button/ButtonIcon'
 import { ValidateUser } from '@/core/repository/account'
 import { rootEstablecimiento } from '@/core/util/routes'
 import { Http } from '@/core/type/enums'
-
+import { IconButton } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu';
 // const inter = Inter({ subsets: ['latin'] })
 
 
@@ -75,12 +76,9 @@ export default function RootLayout({
 
          <div className='w-full overflow-auto  xl:pt-0 h-screen'>
          <div className=' border-b-[0.2px] border-gray-400 p-1 xl:hidden px-2'>
-          <button type="button" className="" onClick={()=>dispatch(uiActions.openSidebar(true))}>
-            <span className="sr-only">Open sidebar</span>
-            <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-            </svg>
-          </button>
+          <IconButton  className="" onClick={()=>dispatch(uiActions.openSidebar(true))}>
+            <MenuIcon/>
+          </IconButton>
           </div>
           
          {children}
