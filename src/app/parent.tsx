34f288952b99dcdbb8f8566ae7e2ac5dfcdedf6b
calrosdsa <carlosduram71@gmail.com>
2024-09-30 14:39:58 -0400

    changes

--- src/app/parent.tsx
+++ src/app/parent.tsx
@@ -53,37 +53,37 @@ export function Parent({ children }:any) {
   },[])
   
 
-  useEffect(()=>{
-    const userLocal = localStorage.getItem("user") 
-    const user:User | null = userLocal != null ? JSON.parse(userLocal) : null
-    if(user != null) {
-      connection.current =  new WebSocket(`${WS_URL}/v1/ws/suscribe/user/admin/?id=${user.id}`)
-      connection.current.onmessage = (e) => {
-        const data:WsAccountPayload = JSON.parse(e.data)
-        switch(data.type){
-          case PayloadType.PAYLOAD_GRUPO_MESSAGES:
-            dispatch(handleIncomeMessages(data.payload))
-            // connection.current?.send("My data")
-            break;
-        }
-        // const payload:MessagePayload = JSON.parse(e.data)
-        // switch(payload.type){
-        //     case MessageEvent.Message:
-        //         const message:ConversationMessage = JSON.parse(payload.payload)
-        //         setMessages(e=>[message,...e])
-        //         break;
+  // useEffect(()=>{
+  //   const userLocal = localStorage.getItem("user") 
+  //   const user:User | null = userLocal != null ? JSON.parse(userLocal) : null
+  //   if(user != null) {
+  //     connection.current =  new WebSocket(`${WS_URL}/v1/ws/suscribe/user/admin/?id=${user.id}`)
+  //     connection.current.onmessage = (e) => {
+  //       const data:WsAccountPayload = JSON.parse(e.data)
+  //       switch(data.type){
+  //         case PayloadType.PAYLOAD_GRUPO_MESSAGES:
+  //           dispatch(handleIncomeMessages(data.payload))
+  //           // connection.current?.send("My data")
+  //           break;
+  //       }
+  //       // const payload:MessagePayload = JSON.parse(e.data)
+  //       // switch(payload.type){
+  //       //     case MessageEvent.Message:
+  //       //         const message:ConversationMessage = JSON.parse(payload.payload)
+  //       //         setMessages(e=>[message,...e])
+  //       //         break;
 
-        // }
-    };
-    connection.current.onclose = () => {
-      console.log("WS ONCLOSE")
-    };
-    }
-    return () => {
-      console.log("WS CLOSE")  
-      connection.current?.close();
-    }
-  },[])
+  //       // }
+  //   };
+  //   connection.current.onclose = () => {
+  //     console.log("WS ONCLOSE")
+  //   };
+  //   }
+  //   return () => {
+  //     console.log("WS CLOSE")  
+  //     connection.current?.close();
+  //   }
+  // },[])
   return (
     <>
      <AppRouterCacheProvider>
